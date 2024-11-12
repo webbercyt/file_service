2a83bedd10f9f7b1c5a3f9030b170be0cb7ccf00
yutao chen <chen.yt1988@outlook.com>
2024-11-12 12:02:26 -0800

    refactor text

--- utils/resource.h
+++ utils/resource.h
@@ -5,7 +5,7 @@ namespace text
 {
 	//infomation
 	static const std::string active_clients					= "active client(s): ";
-	static const std::string connect_closed					= "connnection closed";
+	static const std::string connect_closed					= "succeeded to close";
 	static const std::string connect_succeed				= "succeeded to connect ";
 	static const std::string reconnecting					= "reconnecting ...";
 	static const std::string websocket_handshake_accpted	= "websocket handshake accepted.";
@@ -13,8 +13,8 @@ namespace text
 	//error
 	static const std::string cannot_read_no_name_file	= "cannot read file with empty name.";
 	static const std::string fail_create_dir			= "failed to create directory: ";
-	static const std::string fail_get_file_context		= "failed to get file context. file is opened by other process or path is invalid.";
-	static const std::string fail_open_file				= "failed to open the file.";
+	static const std::string fail_get_file_context		= "failed to get file context: file path is invalid or occupied by others.";
+	static const std::string fail_open_file				= "failed to open file.";
 	static const std::string fail_parse_message			= "failed to parse message";
 	static const std::string filesystem_error			= "filesystem error: ";
 	static const std::string file_over_max_size_limit	= "stop process as file size exceeds max limit: ";
